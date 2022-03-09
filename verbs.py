3590a9a37095ac5294a4d0df3e045f38904415c6
Marc Ace Legaspi <90955780+mecsung@users.noreply.github.com>
2022-03-09 08:31:49 +0800

    Add files via upload

--- verbs.py
+++ verbs.py
@@ -7,6 +7,8 @@ from tkinter import scrolledtext
 from PIL import ImageTk, Image
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import RegexpTokenizer
+import array as arr
+import numpy
 
 mypath = os.path.expanduser('~/nltk_data')
 var = mypath in nltk.data.path
@@ -28,52 +30,190 @@ def convert():
     val = entry.get("1.0", END).lower()
     
     output.insert(END, "\n********************* FILIPINO -UM VERB CHECKER ******************** \n")
-    if tokenizer.tokenize(val):         # check regex if um is present
-        su = val.replace(affix, '')
+    if tokenizer.tokenize(val):             # check regex if um is present
+        su = val.replace(affix, '', 1)
         output.insert(END, (su))
-        # print(su)                     # print salitang ugat
- 
-        if su[:2] == su[2:4]:              # if first two letter is duplicated
-            output.insert(END, (su[2:]))
-            # print(su[2:])
+        
+        length = int(len(su) - 1)
+        if length == 2:                   
+            pass
+        
+        if length == 3:                      
+            if su[:1] == su[1:2]:       
+                output.insert(END, (su[1:]))
+            else:
+                pass
 
-        elif su[:1] == su[1:2]:             # if first letter is duplicated
-            output.insert(END, (su[1:]))
-            # print(su[1:])
+        if length == 4:                        
+            if su[:1] == su[1:2]:            
+                output.insert(END, (su[1:]))
+            else:              
+                pass
+            
+            
+        if length > 4:                       
+            if length == 6:                     
+                if su[:1] == su[1:2]:              
+                    output.insert(END, (su[1:]))
+                 
+                elif su[:2] == su[2:4]:             
+                    output.insert(END, (su[2:]))
+
+                else:
+                    pass
+                    
+            else:       
+                if su[:1] == su[1:2]:
+                    output.insert(END, (su[1:]))
+                        
+                elif su[:2] == su[2:4]: 
+                    output.insert(END, (su[2:]))
+                        
+                elif su[:3] == su[3:6]:  
+                    output.insert(END, (su[3:]))
+                            
+                else:               
+                    pass
               
     else:
         output.insert(END, "unknown value\n")
-        # print("unknown value")
-        
-    # output.insert(END, (val))
+
+
     
 def corpus():
-    ctr, count = 0, 0
-    output.insert(END, "\n***************** -UM AFFIX REMOVAL AND RULE BASED ***************** \n")
+    count, ctr = 0, 0
+    a = 0
+    root_words = []
+    list_words = []
     
+    output.insert(END, "\n***************** -UM AFFIX REMOVAL AND RULE BASED ***************** \n")
+    for word in words:
+        root_words.append(word)
+        
     for token in tokens:
         if tokenizer.tokenize(token):
             output.insert(END, (token) + "\t=\t")
             # print(token, end='  \t')
-            new_token = token.replace(affix, '')
-            
-            # print(token.replace(affix, ''))
-            # print(token.translate(translation))
-            if new_token[:2] == new_token[2:4]:
-                output.insert(END, (new_token[2:]) + "\n")
+            su = token.replace(affix, '', 1)
 
-            elif new_token[:1] == new_token[1:2]:
-                output.insert(END, (new_token[1:]) + "\n")
-
-            else:
-                output.insert(END, (new_token) + "\n")
+            length = int(len(su))
+            if length == 2:                         # umoo --> oo
+                output.insert(END, (su) + "\n")
+                ctr = ctr + 1
+                ugat = su
+                if ugat in root_words:
+                    a = a + 1
+                else:
+                    list_words.append(ugat)
                     
-            count = count + 1
-            for word in words:
-                if word == token.replace(affix, ''):
+            elif length == 3:                      
+                if su[:1] == su[1:2]:               # umooo --> ooo
+                    output.insert(END, (su[1:]) + "\n")                  # ooo --> oo
                     ctr = ctr + 1
+                    ugat = su[1:]
+                    if ugat in root_words:
+                        a = a + 1
+                    else:
+                        list_words.append(ugat)
+                else:                               # umupo --> upo
+                    output.insert(END, (su) + "\n")
+                    ctr = ctr + 1
+                    ugat = su
+                    if ugat in root_words:
+                        a = a + 1
+                    else:
+                        list_words.append(ugat)
 
-    stats = (ctr / count) * 100
+            elif length == 4:                        
+                if su[:1] == su[1:2]:               # umuupo --> uupo
+                    output.insert(END, (su[1:]) + "\n")
+                    ctr = ctr + 1
+                    ugat = su[1:]
+                    if ugat in root_words:
+                        a = a + 1
+                    else:
+                        list_words.append(ugat)
+                else:
+                    output.insert(END, (su) + "\n")                     # bumaba --> baba, kumain --> kain, umutot --> utot
+                    ctr = ctr + 1
+                    ugat = su
+                    if ugat in root_words:
+                        a = a + 1
+                    else:
+                        list_words.append(ugat)
+
+            elif length > 4:                        # bumababa --> bababa, kumakain --> kakain, umuusog --> uusog
+                if length == 6:                     # dumaldal --> daldal
+                    if su[:1] == su[1:2]:               # uusog --> usog
+                        output.insert(END, (su[1:]) + "\n")
+                        ctr = ctr + 1
+                        ugat = su[1:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                     
+                    elif su[:2] == su[2:4]:             # bababa --> baba, kakain --> kain
+                        output.insert(END, (su[2:]) + "\n")
+                        ctr = ctr + 1
+                        ugat = su[2:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                    else:
+                        output.insert(END, (su) + "\n")
+                        ctr = ctr + 1
+                        ugat = su
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                        
+                else:       
+                    if su[:1] == su[1:2]:               # uusog --> usog
+                        output.insert(END, (su[1:]) + "\n")
+                        ctr = ctr + 1
+                        ugat = su[1:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                            
+                    elif su[:2] == su[2:4]:             # bababa --> baba, kakain --> kain
+                        output.insert(END, (su[2:]) + "\n")
+                        ctr = ctr + 1
+                        ugat = su[2:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                            
+                    elif su[:3] == su[3:6]:            # ngingiti --> ngiti
+                        output.insert(END, (su[3:]) + "\n")
+                        ctr = ctr + 1
+                        ugat = su[3:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                                
+                    else:                               # umunawa --> unawa, tumalikod --> talikod
+                        output.insert(END, (su) + "\n")
+                        ctr = ctr + 1
+                        ugat = su
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+               
+            else:
+                output.insert(END, (su) + "Unknown Value\n")
+
+            
+            count = count + 1
+
+    stats = (a / count) * 100
     format_stats = "{:.2f}".format(stats)
     
     # print("\nAccuracy : " + str(format_stats) + " %")
@@ -134,56 +274,207 @@ win.mainloop()
 # ***************************** BACKEND TEST ********************************
 while True:
     count, ctr = 0, 0
+    a = 0
+    root_words = []
+    list_words = []
+    
     print("\nA. -um Corpus\n"
           "B. User Input\n"
           "... Exit\n")
 
     select = (input("Select a task : "))
+    for word in words:
+        root_words.append(word)
+    
     if select.upper() == "A":
         print("\n************** -UM AFFIX REMOVAL ************** \n")
         
         for token in tokens:
             if tokenizer.tokenize(token):
                 print(token, end='  \t')
-                su = token.replace(affix, '')
-                
+                su = token.replace(affix, '', 1)
                 # print(token.translate(translation))
 
-                if su[:2] == su[2:4]:
-                    s2 = su[2:]
-                    print(su[2:])
-
-                elif su[:1] == su[1:2]:
-                    s1 = su[1:]
-                    print(su[1:])
-                else:
+          
+                length = int(len(su))
+                if length == 2:                         # umoo --> oo
                     print(su)
-
-                count = count + 1
-                for word in words:
-                    if word == su:
+                    ctr = ctr + 1
+                    ugat = su
+                    if ugat in root_words:
+                        a = a + 1
+                    else:
+                        list_words.append(ugat)
+                        
+                elif length == 3:                      
+                    if su[:1] == su[1:2]:               # umooo --> ooo
+                        print(su[1:])                   # ooo --> oo
                         ctr = ctr + 1
+                        ugat = su[1:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                    else:                               # umupo --> upo
+                        print(su)
+                        ctr = ctr + 1
+                        ugat = su
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
 
-        stats = (ctr / count) * 100
-        format_stats = "{:.2f}".format(stats)
-        print("\n" + str(ctr))
-        print(count)
-        print("Accuracy : " + str(format_stats) + " %")
+                elif length == 4:                        
+                    if su[:1] == su[1:2]:               # umuupo --> uupo
+                        print(su[1:])
+                        ctr = ctr + 1
+                        ugat = su[1:]
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+                    else:
+                        print(su)                       # bumaba --> baba, kumain --> kain, umutot --> utot
+                        ctr = ctr + 1
+                        ugat = su
+                        if ugat in root_words:
+                            a = a + 1
+                        else:
+                            list_words.append(ugat)
+
+                elif length > 4:                        # bumababa --> bababa, kumakain --> kakain, umuusog --> uusog
+                    if length == 6:                     # dumaldal --> daldal
+                        if su[:1] == su[1:2]:               # uusog --> usog
+                            print(su[1:])
+                            ctr = ctr + 1
+                            ugat = su[1:]
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                         
+                        elif su[:2] == su[2:4]:             # bababa --> baba, kakain --> kain
+                            print(su[2:])
+                            ctr = ctr + 1
+                            ugat = su[2:]
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                        else:
+                            print(su)
+                            ctr = ctr + 1
+                            ugat = su
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                            
+                    else:       
+                        if su[:1] == su[1:2]:               # uusog --> usog
+                            print(su[1:])
+                            ctr = ctr + 1
+                            ugat = su[1:]
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                                
+                        elif su[:2] == su[2:4]:             # bababa --> baba, kakain --> kain
+                            print(su[2:])
+                            ctr = ctr + 1
+                            ugat = su[2:]
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                                
+                        elif su[:3] == su[3:6]:            # ngingiti --> ngiti
+                            print(su[3:])
+                            ctr = ctr + 1
+                            ugat = su[3:]
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                                    
+                        else:                               # umunawa --> unawa, tumalikod --> talikod
+                            print(su)
+                            ctr = ctr + 1
+                            ugat = su
+                            if ugat in root_words:
+                                a = a + 1
+                            else:
+                                list_words.append(ugat)
+                   
+                else:
+                    print('unkown value')
+
+                
+                count = count + 1
+                           
+               
+        for lw in list_words:
+            print(lw)
         
+        stats = (a / count) * 100
+        # print(a)
+        format_stats = "{:.2f}".format(stats)
+        # print("\n" + str(ctr))
+        # print(count)
+        print("Accuracy : " + str(format_stats) + " %")
+
+
     elif select.upper() == "B":
         print("\n********** FILIPINO -UM VERB CHECKER ********** \n")
         while True:
             verb = input("Enter a verb : ")
             if tokenizer.tokenize(verb):        # check regex if um is present
-                su = verb.replace(affix, '')
-                print(su)                       # print salitang ugat
- 
-                if su[:2] == su[2:4]:           # if first two letter is duplicated
-                    print(su[2:])
-
-                elif su[:1] == su[1:2]:            # if first letter is duplicated
-                    print(su[1:])
+                su = verb.replace(affix, '', 1)
                 
+                print(su)                     
+                length = int(len(su))
+                if length == 2:                   
+                    pass
+
+                if length == 3:                      
+                    if su[:1] == su[1:2]:          
+                        print(su[1:])                   
+
+                    else:                           
+                        pass
+
+                elif length == 4:                        
+                    if su[:1] == su[1:2]:            
+                        print(su[1:])
+   
+                    else:
+                        pass
+
+                elif length > 4:                       
+                    if length == 6:                     
+                        if su[:1] == su[1:2]:              
+                            print(su[1:])
+                         
+                        elif su[:2] == su[2:4]:             
+                            print(su[2:])
+
+                        else:
+                            pass
+                            
+                    else:       
+                        if su[:1] == su[1:2]:
+                            print(su[1:])
+                                
+                        elif su[:2] == su[2:4]: 
+                            print(su[2:])
+                                
+                        elif su[:3] == su[3:6]:  
+                            print(su[3:])
+                                    
+                        else:               
+                            pass
+
             else:
                 print("unknown value")
     else:
